48c559edc83ee0bc797a78f65ca4f7222a408ab3
Gustavo e Alec <gnachbar.breve@gmail.com>
2022-03-25 21:42:24 -0300

    Componente App faz o mesmo que o html, mas com JSX

--- react1/src/index.js
+++ react1/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     return (
-        <div>
-            Meu primeiro componente React
+        <div style={{margin: "auto", width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+            <label htmlFor= "nome" style={{display: 'block', marginBottom: 4}}>Nome: </label>
+            <input id='nome' type='text' style={{padding: '8px 0 8px 0', borderStyle: 'hidden', outline: 'none', width: '100%', borderRadius: 8, boxSizing: "border-box"}}>           
+            </input>
+            <button type='submit' style={{marginTop: 12, padding: '8px 0 8px 0', backgroundColor: 'blueviolet', color: 'white',
+            border: 'none', borderRadius: 8, width: '100%'
+        }}>Enviar</button>
         </div>
     )
 }
