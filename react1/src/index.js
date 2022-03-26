3e2a01fc43070d5753d627426a53eed8c3adf9d0
Gustavo e Alec <gnachbar.breve@gmail.com>
2022-03-25 21:56:56 -0300

    adicionando stilos

--- react1/src/index.js
+++ react1/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles.css'
 
 const App = () => {
+    const obterTextoDoBotao = () => "Enviar";
+    const textoDoRotulo = "Nome: ";
+    const estilosBotao = {
+        marginTop: 12, padding: '8px 0 8px 0', backgroundColor: 'blueviolet', color: 'white', 
+        border: 'none', borderRadius: 8, width: '100%'
+    };
+    
     return (
         <div style={{margin: "auto", width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
-            <label htmlFor= "nome" style={{display: 'block', marginBottom: 4}}>Nome: </label>
-            <input id='nome' type='text' style={{padding: '8px 0 8px 0', borderStyle: 'hidden', outline: 'none', width: '100%', borderRadius: 8, boxSizing: "border-box"}}>           
-            </input>
-            <button type='submit' style={{marginTop: 12, padding: '8px 0 8px 0', backgroundColor: 'blueviolet', color: 'white',
-            border: 'none', borderRadius: 8, width: '100%'
-        }}>Enviar</button>
+            <label className="rotulo" htmlFor= "nome" style={{display: 'block', marginBottom: 4}}>{textoDoRotulo}</label>
+            <input id='nome' type='text' style={{padding: '8px 0 8px 0', borderStyle: 'hidden', outline: 'none', width: '100%', borderRadius: 8, boxSizing: "border-box"}}></input>
+            <button type='submit' style={estilosBotao}>{obterTextoDoBotao()}</button>
         </div>
     )
 }
